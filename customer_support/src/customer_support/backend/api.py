9acdc0b9e31cf717e7dea091367a59ab285a7fbd
Omer Aytug <aytugomer00@gmail.com>
2026-04-20 11:32:58 +0200

    setting up middlewares

--- customer_support/src/customer_support/backend/api.py
+++ customer_support/src/customer_support/backend/api.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from customer_support.backend.agent import support_agent
+from customer_support.backend.middlewares import logging_middleware
 
 app = FastAPI()
+logging_middleware(app=app)
 
 # get for simplicity -> in real cases use post
 @app.get("/customer_support")
--- customer_support/src/customer_support/backend/constants.py
+++ customer_support/src/customer_support/backend/constants.py
@@ -11,3 +11,5 @@ LLM_JUDGE="openrouter:/nvidia/nemotron-3-nano-30b-a3b:free"
 MONITORING_PATH = Path(__file__).parents[1] / "monitoring"
 
 mlflow.set_tracking_uri(f"sqlite:///{MONITORING_PATH / 'mlflow.db'}")
+mlflow.set_experiment("customer_support_bot")
+mlflow.pydantic_ai.autolog()
